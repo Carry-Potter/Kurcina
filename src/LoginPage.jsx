2f8196b2c215ae49c9988313db93dcbca388aacd
Ivan Jelic <52745375+Carry-Potter@users.noreply.github.com>
2024-11-25 11:45:48 +0100

    Update LoginPage.jsx

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -64,7 +64,7 @@ export default function LoginPage({ setUser }) {
         <button
           type="button"
           onClick={() => {
-            window.open('http://localhost:5000/auth/google', '_self');
+            window.open('https://frizerski-bekend.onrender.com/auth/google', '_self');
           }}
           className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
         >
